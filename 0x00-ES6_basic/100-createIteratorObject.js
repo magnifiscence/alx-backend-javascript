c1ead8a6691643c063822ca5a4c63a6c695d109e
tata <tdivine284@gmail.com>
2024-03-05 02:55:34 +0000

    task done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,28 +1,17 @@
 export default function createIteratorObject(report) {
-    let departments = Object.keys(report);
+  let employees = [];
+  for (const dept in report) {
+    employees = employees.concat(report[dept]);
+  }
 
-    let currentDepartmentIndex = 0;
-    let currentEmployeeIndex = 0;
+  let index = 0;
 
-    return {
-        next: function() {
-            if (currentDepartmentIndex < departments.length) {
-                let department = departments[currentDepartmentIndex];
-                let employees = report[department].employees;
-
-                if (currentEmployeeIndex < employees.length) {
-                    let employee = employees[currentEmployeeIndex];
-                    currentEmployeeIndex++;
-                    return { value: employee, done: false };
-                } else {
-                    currentDepartmentIndex++;
-                    currentEmployeeIndex = 0;
-                    return this.next();
-                }
-            } else {
-                return { done: true };
-            }
-        }
-    };
+  return {
+    next: function() {
+      return {
+        value: employees[index],
+        done: index++ >= employees.length
+      };
+    }
+  };
 }
-
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-    let iterator = reportWithIterator.next();
-
-    while (!iterator.done) {
-        result += iterator.value.join(' | ');
-        iterator = reportWithIterator.next();
-        if (!iterator.done) {
-            result += ' | ';
-        }
-    }
-
-    return result;
+  let result = '';
+  for (const item of reportWithIterator) {
+    result += item + ' | ';
+  }
+  // Remove the trailing ' | ' from the result
+  return result.slice(0, -3);
 }
-
