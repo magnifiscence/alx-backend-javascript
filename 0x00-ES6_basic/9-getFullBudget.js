2270d4c526cffaae0df77ed7728d9bf80f93873f
tata <tdivine284@gmail.com>
2024-03-05 02:45:04 +0000

    task done

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars (income) {
-      return `$${income}`;
+    getIncomeInDollars() {
+      return `$${this.income}`;
     },
-    getIncomeInEuros (income) {
-      return `${income} euros`;
+    getIncomeInEuros() {
+      return `${this.income} euros`;
     }
   };
 
